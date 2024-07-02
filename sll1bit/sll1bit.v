521d11e4806c6fe2316bf911b99a168b9f36fab8
YarethB <161420975+YarethB@users.noreply.github.com>
2024-07-01 19:13:31 -0600

    Update sll1bit.v

--- sll1bit/sll1bit.v
+++ sll1bit/sll1bit.v
@@ -1,3 +1,11 @@
+	/*	Grupo: 5CV3 	Proyecto: Monociclo	Archivo: sll1bit.v
+Equipo: 6
+Integrantes: 
+Díaz Ortiz Brandon Aldair			González Rosales Brenda Yareth
+Hernández Suarez Diego Armando			Pérez Aguilar Dulce Evelyn 
+Romero Barrientos Jonathan Rubén
+Descripción:  desplazamiento lógico a la izquierda de un bit en una palabra de 32 bits de entrada 
+*/
 module sll1bit(
 	input [31:0] entrada_i,
 	output [31:0] salida_o
